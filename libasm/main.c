22f76353fc2d7e7988b45734e35bec2a27e3d78f
loaki <jules.feuillatre@gmail.com>
2020-03-04 14:32:52 +0100

    makefile

--- libasm/main.c
+++ libasm/main.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jfeuilla <jfeuilla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/03/03 18:01:56 by jfeuilla          #+#    #+#             */
-/*   Updated: 2020/03/03 19:33:45 by jfeuilla         ###   ########.fr       */
+/*   Created: 2020/03/04 14:15:50 by jfeuilla          #+#    #+#             */
+/*   Updated: 2020/03/04 14:30:53 by jfeuilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,13 @@
 #include <string.h>
 #include <stdio.h>
 
-# define STRLEN(x)			printf("%s = %d, exp %d\n", x, ft_strlen(x), (int)strlen(x));
+# define STRLEN(x)			printf("%3s = %d, exp %d\n", x, ft_strlen(x), (int)strlen(x));
 
 int		ft_strlen(char const *str);
 
 int main()
 {
 	printf("\n---TESTS---\n");
-	STRLEN("salut");
+	STRLEN("yo");
+	STRLEN("");
 }
